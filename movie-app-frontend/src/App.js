c3fbdf9659e53c802afc099b0cb02799e176e978
ashishshah1995 <shahashish991@gmail.com>
2025-06-03 10:17:47 +1000

    add react files

---
+++ movie-app-frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MovieList from "./components/MovieList";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Webjet Movie Price Comparison</h1>
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
